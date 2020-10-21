a642eb22b0c8caeaec7cab7311e3bd26f72f3f02
Frank McAuley <frank.mcauley@gmail.com>
2020-10-21 13:06:39 -0400

    Initial Commit

---
+++ BinarySearch/BinarySearch.h
@@ -0,0 +1,18 @@
+//
+//  BinarySearch.h
+//  BinarySearch
+//
+//  Created by Frank McAuley on 10/21/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BinarySearch.
+FOUNDATION_EXPORT double BinarySearchVersionNumber;
+
+//! Project version string for BinarySearch.
+FOUNDATION_EXPORT const unsigned char BinarySearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BinarySearch/PublicHeader.h>
+
+
